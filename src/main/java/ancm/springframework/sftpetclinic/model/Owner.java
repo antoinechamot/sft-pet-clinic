d7c5ed97eed6ed6dd240e03adf4b3908faf5b56e
tchame44 <chamot.antoine@gmail.com>
2019-02-20 20:26:03 +0100

    Add pojo

---
+++ src/main/java/ancm/springframework/sftpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package ancm.springframework.sftpetclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ src/main/java/ancm/springframework/sftpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package ancm.springframework.sftpetclinic.model;
+
+public class Vet extends Person{
+
+}
