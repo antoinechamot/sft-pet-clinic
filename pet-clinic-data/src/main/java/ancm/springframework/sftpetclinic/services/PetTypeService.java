2f7561f90df3323c430d537f0d2961b5e04fa06f
tchame44 <chamot.antoine@gmail.com>
2019-03-10 22:45:05 +0100

    Create PetType Map Service Impl closes #34

---
+++ pet-clinic-data/src/main/java/ancm/springframework/sftpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package ancm.springframework.sftpetclinic.services;
+
+import ancm.springframework.sftpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/ancm/springframework/sftpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,12 @@
+package ancm.springframework.sftpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+
+import ancm.springframework.sftpetclinic.model.PetType;
+import ancm.springframework.sftpetclinic.services.PetTypeService;
+
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService{
+
+}
