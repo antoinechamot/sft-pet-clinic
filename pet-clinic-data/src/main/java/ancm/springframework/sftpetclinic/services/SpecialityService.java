7b446345e79323808053330f48713823f6dd5061
tchame44 <chamot.antoine@gmail.com>
2019-03-11 20:50:32 +0100

    Create Specialty Map Service Impl closes #36

---
+++ pet-clinic-data/src/main/java/ancm/springframework/sftpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package ancm.springframework.sftpetclinic.services;
+
+import ancm.springframework.sftpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/ancm/springframework/sftpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,10 @@
+package ancm.springframework.sftpetclinic.services.map;
+
+import ancm.springframework.sftpetclinic.model.Speciality;
+import ancm.springframework.sftpetclinic.services.SpecialityService;
+
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService{
+
+	
+
+}
