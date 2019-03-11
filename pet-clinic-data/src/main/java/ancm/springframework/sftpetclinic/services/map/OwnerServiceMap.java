c80439c76e6e06c5082cecaa82e6521677afb271
tchame44 <chamot.antoine@gmail.com>
2019-03-11 20:38:44 +0100

    Enhance Owners with contactInfo and Pets on startup closes #38

--- pet-clinic-data/src/main/java/ancm/springframework/sftpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/ancm/springframework/sftpetclinic/services/map/OwnerServiceMap.java
@@ -3,11 +3,65 @@ package ancm.springframework.sftpetclinic.services.map;
 import org.springframework.stereotype.Service;
 
 import ancm.springframework.sftpetclinic.model.Owner;
+import ancm.springframework.sftpetclinic.model.Pet;
 import ancm.springframework.sftpetclinic.services.OwnerService;
+import ancm.springframework.sftpetclinic.services.PetService;
+import ancm.springframework.sftpetclinic.services.PetTypeService;
 
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService{
+
+	
+	private final PetTypeService petTypeService;
+	private final PetService petService;
+
+	
+	
+
+	public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+		super();
+		this.petTypeService = petTypeService;
+		this.petService = petService;
+	}
+
+
+
+	
+
+	@Override
+	public Owner save(Owner object) {
+		
+		Owner savedOwner = null;
+		
+		if(object != null) {
+			
+			object.getPets().forEach(pet -> {
+				
+				if(pet.getPetType() != null) {
+					
+					if(pet.getPetType().getId() == null) {
+						pet.setPetType(petTypeService.save(pet.getPetType())); 
+					}
+					
+				} else {
+					throw new RuntimeException("Pet Type is Required");
+				}
+				
+				if(pet.getId() == null) {
+					Pet savedPet = petService.save(pet);
+					pet.setId(savedPet.getId());
+				}
+			});
+			
+			return super.save(object);
+		}
+		
+		return savedOwner;
+		
+	}
+
+
 
 
 
--- pet-clinic-web/src/main/java/ancm/springframework/sftpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ancm/springframework/sftpetclinic/bootstrap/DataLoader.java
@@ -1,9 +1,12 @@
 package ancm.springframework.sftpetclinic.bootstrap;
 
+import java.time.LocalDate;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import ancm.springframework.sftpetclinic.model.Owner;
+import ancm.springframework.sftpetclinic.model.Pet;
 import ancm.springframework.sftpetclinic.model.PetType;
 import ancm.springframework.sftpetclinic.model.Vet;
 import ancm.springframework.sftpetclinic.services.OwnerService;
@@ -45,11 +48,36 @@ public class DataLoader implements CommandLineRunner {
 		Owner owner1 = new Owner();
 		owner1.setFirstName("toto");
 		owner1.setLastName("toto2");
+		owner1.setAddress("123 Street");
+		owner1.setCity("city1");
+		owner1.setTelephone("tel1");
+		
+		Pet pet1 = new Pet();
+		pet1.setPetType(savedDogPetType);
+		pet1.setOwner(owner1);
+		pet1.setBirthDate(LocalDate.now());
+		pet1.setName("pet1Name");
+		owner1.getPets().add(pet1);
+		
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
 		owner2.setFirstName("tata");
 		owner2.setLastName("tata2");
+		owner2.setAddress("123 Street");
+		owner2.setCity("city2");
+		owner2.setTelephone("tel2");
+		
+		
+
+		Pet pet2 = new Pet();
+		pet2.setPetType(savedCatPetType);
+		pet2.setOwner(owner2);
+		pet2.setBirthDate(LocalDate.now());
+		pet2.setName("pet2Name");
+		owner2.getPets().add(pet2);
+		
+		
 		ownerService.save(owner2);
 		
 	   System.out.println("Owners Loaded");
