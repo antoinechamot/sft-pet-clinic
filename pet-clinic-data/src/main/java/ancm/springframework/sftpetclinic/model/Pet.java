36906132c4b85ee0d8f9cc1a937b0af99d90ae5c
tchame44 <chamot.antoine@gmail.com>
2019-03-16 20:42:31 +0100

    Create Visit Entity closes #43

--- pet-clinic-data/src/main/java/ancm/springframework/sftpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/ancm/springframework/sftpetclinic/model/Pet.java
@@ -1,12 +1,15 @@
 package ancm.springframework.sftpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -23,6 +26,8 @@ public class Pet extends BaseEntity{
 	private Owner owner;
 	@Column(name="birth_date")
 	private LocalDate birthDate;
+	@OneToMany(cascade=CascadeType.ALL,mappedBy="pet")
+	private Set<Visit> visits = new HashSet<Visit>();
 	
 	
 	public PetType getPetType() {
@@ -48,6 +53,12 @@ public class Pet extends BaseEntity{
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 
 }
